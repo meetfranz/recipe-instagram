5b512b5765ea6d3e4eb290f3b5114f70506aa111
Stefan Malzner <stefan@adlk.io>
2020-04-13 09:48:50 +0200

    initial commit

---
+++ webview.js
@@ -0,0 +1,15 @@
+import path from 'path';
+
+module.exports = (Franz) => {
+  const getMessages = function getMessages() {
+    const element = document.querySelector('a[href^="/direct/inbox"]');
+
+    if (element) {
+      Franz.setBadge(parseInt(element.innerText, 10));
+    }
+  };
+
+  Franz.loop(getMessages);
+
+  Franz.injectCSS(path.join(__dirname, 'service.css'));
+};
